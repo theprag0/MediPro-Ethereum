67d3a394e5a9d0eb3b6773ec360fb7159a5d840f
Praghadiesh Saravanan <68212668+theprag0@users.noreply.github.com>
2023-03-29 21:34:43 +0530

    add env

--- pages/doctor.js
+++ pages/doctor.js
@@ -24,8 +24,8 @@ function Doctor() {
     const [alertOpen, setAlertOpen] = useState(false);
     const [alertMsg, setAlertMsg] = useState('');
 
-    const INFURA_ID = "2MdNw6HIEDfZbe2luScQn8tw3zK";
-    const INFURA_SECRET_KEY = "3c0ce65e22dac309da46a47320841738";
+    const INFURA_ID = process.env.INFURA_ID;
+    const INFURA_SECRET_KEY = process.env.INFURA_SECRET_KEY;
     const auth = 'Basic ' + Buffer.from(INFURA_ID + ':' + INFURA_SECRET_KEY).toString('base64');
 
     const ipfs = create(
